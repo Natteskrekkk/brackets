7000184bfe45eeded924f16e395552720243caea
Timofey Gribanov <timofey.gribanov@gmail.com>
2023-02-27 18:37:08 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,41 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const stack = [];
+  for (let char of str) {
+    const last = stack.at(-1);
+    if (isClosing(char, bracketsConfig) && canClose(last, char, bracketsConfig)) {
+      stack.pop();
+    } else if (isOpening(char, bracketsConfig)) {
+      stack.push(char);
+    } else {
+      return false;
+    }
+  }
+  return stack.length === 0;
+}
+
+function isClosing(char, config) {
+  for (const pair of config) {
+    if (pair[1] === char) {
+      return true;
+    }
+  }
+  return false;
+}
+
+function isOpening(char, config) {
+  for (const pair of config) {
+    if (pair[0] === char) {
+      return true;
+    }
+  }
+  return false;
+}
+
+function canClose(opening, closing, config) {
+  for (const pair of config) {
+    if (pair[0] === opening) {
+      return pair[1] === closing;
+    }
+  }
+  return false;
 }
